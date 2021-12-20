a238c52bb41e89cdbcb5803e0b63cec3d34eab5b
XinghMp968 <31666974+singhmp968@users.noreply.github.com>
2021-12-20 20:24:08 +0530

    Dispatch Actiions toturial

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,27 @@
-
+import React from "react";
+import { data } from '../data'
+import Navbar from './Navbar';
+import MovieCard from './MovieCard'
+import movies from "../reducers";
 function App() {
   return (
     <div className="App">
-        Project Setup
+        <Navbar />
+        <div className="main">
+              <div className="tabs">
+                    <div className="tab">Movies</div>
+                    <div className="tab">Favourites</div>
+              </div>
+              <div className="lsit">
+                {data.map((movie,index) =>(
+                  //  passing movie data via props
+                  //console.log('movie',movie)
+
+                  <MovieCard movie={movie} key={`movies-${index}`}  />
+                ))}
+              </div>
+
+        </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+
 import './index.css';
 import App from './components/App';
+import movies from './reducers'
+// in creasteStore method we need to pass reducers
+const store = createStore(movies); // createStore this is being to us by redux 
+console.log('store',store);
+console.log('Before state',store.getState());
+
+store.dispatch({
+  // this action object
+  type:'ADD_MOVIES',
+  movies: [{name:'Superman'}] //  passing the movie to be added
+}); // it is used to send action in the store
+console.log('After state',store.getState());
 
 ReactDOM.render(
   <React.StrictMode>

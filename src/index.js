16adf1b2b44a520d10f79c1bbb3d1e375d752ad1
XinghMp968 <31666974+singhmp968@users.noreply.github.com>
2021-12-26 11:40:24 +0530

    added middleare concepts

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from 'redux';
+import { createStore,applyMiddleware } from 'redux';
 
 import './index.css';
 import App from './components/App';
@@ -9,10 +9,34 @@ import App from './components/App';
 import rootReducer from './reducers';
 import combineReducers from './reducers';
 
+
+// creating a middleware
+// curried form of logge({obj,next,action})
+// logger(obj)(next)(action)
+/*
+const logger = function({dispatch,getState}){ // obj will contain disapatch and getStat propety
+  return function(next){
+    return function(action){
+      // middle ware code
+      console.log('ACTION_TYPE = ',action.type)
+      next(action); // if we dont call this the our app got stuck here and  this willl be equlivant to calling that middle ware
+    }
+  }
+}
+*/
+//**  converting the above function into arrow function using urring concept
+// 2nd way of wirting middlware
+const logger = ({dispatch,getState})=>(next)=>(action)=>{
+    // middle ware code
+    console.log('ACTION_TYPE = ',action.type);
+    next(action); // if we dont call this the our app got stuck here and  this willl be equlivant to calling that middle ware
+
+    
+}
 // in creasteStore method we need to pass reducers
 //const store = createStore(movies); // createStore this is being to us by redux 
 // here we are now passing the root reducers
-const store = createStore(combineReducers); // createStore this is being to us by redux 
+const store = createStore(combineReducers ,applyMiddleware(logger)); // createStore this is being to us by redux 
 
 console.log('store',store);
 /*
